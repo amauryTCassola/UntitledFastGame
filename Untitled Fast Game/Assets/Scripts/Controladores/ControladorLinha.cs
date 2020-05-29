350aaef76c60738ebe3b292b68477c8801825b51
amauryTCassola <amaury_cassola@yahoo.com.br>
2020-05-29 19:43:49 -0300

    'arrumei' o bug da linha

--- Untitled Fast Game/Assets/Scripts/Controladores/ControladorLinha.cs
+++ Untitled Fast Game/Assets/Scripts/Controladores/ControladorLinha.cs
@@ -5,25 +5,27 @@ using UnityEngine;
 public class ControladorLinha{
     LineRenderer lRend;
     GameObject rendererObject;
+    List<Vector3> listaPontos;
 
     public ControladorLinha()
     {
         rendererObject = (GameObject)Object.Instantiate(Resources.Load("DesenhadorDeLinha"));
         lRend = rendererObject.GetComponent<LineRenderer>();
+        listaPontos = new List<Vector3>();
 
     }
 
     //Retorna os pontos da lista de pontos do line renderer em forma de List<Vector3>
     List<Vector3> GetListaDePontos(){
-        Vector3[] positions = new Vector3[lRend.positionCount];
-        lRend.GetPositions(positions);
-        return new List<Vector3>(positions);
+        return listaPontos;
     }
 
     //Dada uma List<Vector3>, transforma a lista em array e a insere no lineRenderer
     void SetListaDePontos(List<Vector3> pontosLista){
-        lRend.positionCount = pontosLista.Count;
-        lRend.SetPositions(pontosLista.ToArray());
+        listaPontos = pontosLista;
+        Vector3[] arrayDePontosBugFixed = LineRendererBugfix(listaPontos.ToArray());
+        lRend.positionCount = arrayDePontosBugFixed.Length;
+        lRend.SetPositions(arrayDePontosBugFixed);
     }
 
     //Dado um ponto representado por um Vector3, se este ponto faz parte da lista de pontos
@@ -57,6 +59,7 @@ public class ControladorLinha{
     //Substitui a lista de pontos atual por uma vazia
     public void ApagaTodosOsPontos(){
         lRend.positionCount = 0;
+        listaPontos.Clear();
     }
 
     //Retorna o último ponto da lista
@@ -64,5 +67,26 @@ public class ControladorLinha{
         List<Vector3> listaPontos = GetListaDePontos();
         return listaPontos[listaPontos.Count-1];
     }
+
+    Vector3[] LineRendererBugfix(Vector3[] original)
+     {
+         Vector3[] res = new Vector3[original.Length * 3 - 2];
+         for (int i = 0; i < res.Length; i++)
+         {
+             if (i % 3 == 0)
+             {
+                 res[i] = original[i / 3];
+             }
+             else if (i % 3 == 1)
+             {
+                 res[i] = Vector3.Lerp(original[(i - 1) / 3], original[(i + 2) / 3], 0.1f);
+             }
+             else if (i % 3 == 2)
+             {
+                 res[i] = Vector3.Lerp(original[(i + 1) / 3], original[(i - 2) / 3], 0.1f);
+             }
+         }
+         return res;
+     }
 
 }
