18fc7a0489bd6bfad018a6120ca5c1cf12cb7bd5
unknown <vinifraga549@gmail.com>
2020-07-20 13:37:59 -0300

    nao lembro

---
+++ Untitled Fast Game/Assets/Scripts/Ataque/Respawn.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.SceneManagement;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    GameObject player;
+    public List<Object> inimigosPref;
+    Vector2 posicaoPlayer;
+    List<GameObject> inimigos;
+    List<Vector2> posicoes;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        inimigosPref = new List<Object>(Resources.LoadAll("prefabs/inimigos"));
+        inimigos = new List<GameObject>(GameObject.FindGameObjectsWithTag("Inimigo"));
+        posicoes = new List<Vector2>();
+    }
+
+    void Respawnar()
+    {
+
+    }
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
